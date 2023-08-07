95bd7c606d278662b1b6159a570616e2897e6dca
jcarbolynn <36271436+jcarbolynn@users.noreply.github.com>
2023-08-07 14:58:58 -0400

    Update main.py
    more playing around with tkinter
    title, sizing, label, button, entry, text, spin box, sliding scale, checked, radio, list box

--- main.py
+++ main.py
@@ -1,46 +1,86 @@
-import tkinter
+from tkinter import *
 
-# initialize new object from this class
-window = tkinter.Tk()
+#Creating a new window and configurations
+window = Tk()
+window.title("Widget Examples")
+window.minsize(width=500, height=500)
 
-# # need to keep window open so use while loop, built in as main loop
-# keep at bottom of code
-# window.mainloop()
+#Labels
+label = Label(text="This is old text")
+label.config(text="This is new text")
+label.pack()
 
-window.title("My first GUI")
-window.minsize(width = 400, height = 300)
-window.config(padx=20, pady=20)
+#Buttons
+def action():
+    print("Do something")
 
-# create a label using the label class
-# things wont show up without specifyin ghow it will be displayed
-my_label = tkinter.Label(text="I am a label", font = ("arial", 24, "italic"))
-# pack places things into screen in center
-my_label.grid(column=0,row=0)
-# pack comes with its own set of arguments
-# my_label.pack(expand=True)
-# pack does not show all arguments available in documentation: **kw
-my_label.config(padx=0, pady=100)
+#calls action() when pressed
+button = Button(text="Click Me", command=action)
+button.pack()
 
-# how to change label text:
-# my_label["text"] = "New Text"
-# pass in new text as keyword argument
-# my_label.config(text='New Text')
+#Entries
+entry = Entry(width=30)
+#Add some text to begin with
+entry.insert(END, string="Some text to begin with.")
+#Gets text in entry
+print(entry.get())
+entry.pack()
 
-def button_clicked():
-  my_label.config(text=input.get())
-  # my_label.config(text='I was clicked')
-  # print("I was clicked")
+#Text
+text = Text(height=5, width=30)
+#Puts cursor in textbox.
+text.focus()
+#Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+#Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
 
-# when button detects command, triggers this method, no () because we only want to call the function when the button is clicked
-button = tkinter.Button(text= 'click me', command=button_clicked)
-button.grid(column=1,row=1)
+#Spinbox
+def spinbox_used():
+    #gets the current value in spinbox.
+    print(spinbox.get())
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
 
-another_button = tkinter.Button(text="new butt")
-another_button.grid(column=3,row=0)
+#Scale
+#Called with current scale value.
+def scale_used(value):
+    print(value)
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
 
-# entry component
-input = tkinter.Entry()
-input.grid(column=4,row=3)
-# get() returns entry string
+#Checkbutton
+def checkbutton_used():
+    #Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+#variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = IntVar()
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
 
+#Radiobutton
+def radio_used():
+    print(radio_state.get())
+#Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+#Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
 window.mainloop()
+
